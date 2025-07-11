1e2ffe7becf3814e50798e13a2c398bac7303da1
gengsz <igengsz@163.com>
2025-07-11 17:14:42 +0800

    init

---
+++ src/Plugin/Traits/SplitTableResolver.php
@@ -0,0 +1,23 @@
+<?php
+namespace Plugin\Traits;
+
+trait SplitTableResolver {
+    protected static function resolveModel($uid) {
+        $modelPrefix = static::modelPrefix(); // 子类提供，比如 'KPTask'
+        $hash = static::tableHash($uid);     // 可换成任意逻辑
+        $class = $modelPrefix . $hash;
+
+        if (!class_exists($modelPrefix.'00')) {
+            require_once __DIR__ . "/{$modelPrefix}00.php";
+        }
+
+        return $class;
+    }
+
+    protected static function tableHash($uid) {
+        return str_pad($uid % 100, 2, '0', STR_PAD_LEFT); // 默认分100张表
+    }
+
+    // 必须由子类实现
+    abstract protected static function modelPrefix();
+}
